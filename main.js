521e35ddffbcd2fb5d496bbc1085ea31d81383e1
yth98 <b06901031@ntu.edu.tw>
2020-10-06 10:21:17 +0800

    boilerplate

--- main.js
+++ main.js
@@ -5,13 +5,37 @@ var button = document.getElementById("cal-button")
 input.value = ''
 color.value = '#b0b0b0'
 
+var activeCell;
 
+button.onclick = () => {
+    if(activeCell instanceof HTMLElement) {
+        something = document.createElement('div');
+        something.innerText = input.value;
+        something.style.color = color.value;
+        activeCell.appendChild(something);
+    }
+}
 
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter') button.onclick();
+});
 
+const selectCell = (e) => {
+    activeCell = e.target;
+    for(cell of cells) {
+        cell.classList.remove('active-date');
+    }
+    e.target.classList.add('active-date');
+    input.value = '';
+}
 
-
-
-
+window.onload = () => {
+    for(cell of cells) {
+        cell.onclick = selectCell;
+    }
+    activeCell = cells[0];
+    cells[0].classList.add('active-date');
+}
 
 //Sets the page's theme. No need to modify
 var themeButton = document.getElementsByClassName("ChooseTheme")
